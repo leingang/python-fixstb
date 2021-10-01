6ec7209e5ba949591d62626ff0fed9dcbf4dfcb8
Matthew Leingang <leingang@nyu.edu>
2021-10-01 07:54:45 -0400

    Use pytest for regression testing

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='yourscript',
+    name='fixstb',
     version='0.1.0',
     py_modules=['fixstb'],
     install_requires=[
         'Click','latex2mathml'
     ],
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'fixstb = fixstb:cli',
